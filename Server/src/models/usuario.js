043f1e753857aa9969e68945bfa9e04dff5ad559
AlterDoc <diego-cordara@hotmail.com>
2023-12-04 21:12:06 -0300

    primer commit doc

---
+++ Server/src/models/usuario.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    } }, { timestamps: false });
+};
